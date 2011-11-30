7e256389f9ec68a1a2800056c638a5f00edbb158
Christian T <c.theilemann@gmail.com>
2011-11-30 04:22:52 +0100

    Sehr grobes UI Gerüst des Patientendashboards steht -> Weiter gehts mit Integration von Andreas kram in meinen

---
+++ neq_MobileClient/app/view/testpanel.js
@@ -0,0 +1,72 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: geekflyer
+ * Date: 30.11.11
+ * Time: 03:45
+ * To change this template use File | Settings | File Templates.
+ */
+/**
+ * Created by JetBrains WebStorm.
+ * User: geekflyer
+ * Date: 29.11.11
+ * Time: 20:44
+ * To change this template use File | Settings | File Templates.
+ */
+/**
+ * @author geekflyer
+ */
+
+Ext.define('NeqMobile.view.Testpanel', {
+        extend:'Ext.Panel',
+        alias:'widget.testpanel',
+
+        config:{
+
+           
+            html: 'du stinkst'
+
+//                    xtype : 'formpanel',
+//                    items: [
+//                        {
+//                            xtype: 'fieldset',
+//
+//
+//                            title: 'Basic Data',
+//                            items: [
+//
+//                                {
+//
+//                                    xtype: 'textfield',
+//                                    name: 'firstName',
+//                                    label: 'First Name'
+//                                },
+//                                {
+//                                    xtype: 'textfield',
+//                                    name: 'lastName',
+//                                    label: 'Last Name'
+//                                },
+//                                {
+//                                    xtype: 'textfield',
+//                                    name: 'age',
+//                                    label: 'Age'
+//                                },
+//                                {   xtype: 'textareafield',
+//                                    name: 'adress',
+//                                    label: 'Adresse'
+//                                }, {xtype: 'button',
+//    text: 'blaa'
+//
+//
+//
+
+
+
+
+
+
+
+
+        }}
+
+
+);
