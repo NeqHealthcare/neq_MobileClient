af02999785eb81e1fbd5c604c69ba79fd7b9c92a
Christian T <c.theilemann@gmail.com>
2012-02-02 02:36:37 +0100

    bla

--- neq_MobileClient/app/view/patient/List.js
+++ neq_MobileClient/app/view/patient/List.js
@@ -37,8 +37,13 @@ Ext.define('NeqMobile.view.patient.List', {
                 // grouped     : true,
                 //  indexBar    : true,
                 flex:1,
-               // store:'NeqMobile.store.Patients',
-                itemTpl:'{id}  <strong>{rec_name}</strong>'
+                // store:'NeqMobile.store.Patients',
+                itemTpl:'{id}  <strong>{rec_name}</strong>' +
+                    '<br>' +
+                    'age: {[values.age.split(" ")\[0\]]} ' +
+                    //'{[myage.slice(0,myage.length - 1)]}' +
+                    ' actual disease: {latestDiagnoseRecName}' +
+                    '  sex: {sex}'
             }
         ]
     }
