17be6c9713a548cbfe1110fc736bdc94c4d9f116
Andreas Fleisch <and.fleisch@gmail.com>
2012-02-02 02:56:26 +0100

    new css - login liste angepassst

--- neq_MobileClient/app/view/Login.js
+++ neq_MobileClient/app/view/Login.js
@@ -6,9 +6,8 @@ Ext.define('NeqMobile.view.Login', {
     extend:'Ext.Container',
     requires:['NeqMobile.store.Domains', 'NeqMobile.store.Patients'],
     xtype:'Login',
-    cls: 'login',
     config:{
-
+        cls: 'login',
         items:[
 
             {
@@ -37,7 +36,7 @@ Ext.define('NeqMobile.view.Login', {
                 //centered:'true',
                 //pack: 'start',
                 //ui:'login',
-                StyleHtmlContent: true,
+                //StyleHtmlContent: true,
                 cls:'loginform',
                 //style:'background-color: white;',
                 scrollable:false,
