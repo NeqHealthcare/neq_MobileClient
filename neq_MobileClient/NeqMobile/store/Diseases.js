f43bef89c9f72c5118b214f44bbae5e54d87dc71
unknown <geekflyer@blubmobile.(none)>
2012-02-15 00:15:25 +0100

    implementation of dashboard webservice usage - first try in controller logic - has not been tested

---
+++ neq_MobileClient/NeqMobile/store/Diseases.js
@@ -0,0 +1,19 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: geekflyer
+ * Date: 14.02.12
+ * Time: 19:09
+ * To change this template use File | Settings | File Templates.
+ */
+Ext.define('NeqMobile.store.Diagnoses', {
+        extend:'Ext.data.Store',
+        requires:'NeqMobile.model.Diagnose',
+
+
+        config:{model:'NeqMobile.model.Diagnose',
+            //  sorters: 'lastName',
+//            autoLoad:true,
+//            autoSync:true,
+            storeId:'diagnoses'
+        }}
+);
