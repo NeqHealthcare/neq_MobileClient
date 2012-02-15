92f325c6ed568ece9ff7e58975f9445ead299cee
Joohee <joohee@joohee-Q210-P210.(none)>
2012-02-15 14:15:23 +0100

    die untere tabelle is nicht 100% fertig, da das problem mit store noch nicht behoben ist. aber die obere tabelle sollte so passen...habs so geändert wie die sketches von jan

--- neq_MobileClient/NeqMobile/view/patient/Info.js
+++ neq_MobileClient/NeqMobile/view/patient/Info.js
@@ -10,21 +10,23 @@
  */
 
 var patientheader = new Ext.XTemplate(
-//    '<h1>"Information"</h1><table border="2" cellspacing="10">' +
-//        '</table>'
-
-    //'<h1>here should be the patient header</h1>'
-    '<div class="patientImage" style="float: left; height: 125px; width: 114px; margin-right: 10px; background-size: cover; background-position: center center; background: #ddd; @include border-radius(3px); -webkit-box-shadow: inset 0 0 2px rgba(0,0,0,.6); background-image:url(theme/images/user/DefaultAvatar_small.jpg);"></div>',
-    //'<div class="headshot" style="background-image:url(resources/images/headshots/{headshot});"></div>',
-    '<span style="display: block; font-size: 12pt; font-weight: bold; color: #000;">ID: {id} - {rec_name}</strong>  - {[values.age.split(" ")\[0\]]} - {sex}&nbsp;</span>',
-    '<span style="display: block; font-size: 12pt; font-weight: normal; color: #666;">{latestDiagnoseRecName}&nbsp;</span>',
-    '<br /><br />'
+    '<h3>Patient</h3><table cellpadding="5" cellspacing="10" style="background-color: #FFFFFF; -webkit-border-radius: 6px";>' +
+        '<tr>'+
+        '<th rowspan="2" width=120><img src="theme/images/user/DefaultAvatar_small.jpg" width="114" height="125"></th>'+
+        '   <td align="left"><b>Name:</b> {id}</td></tr>'+
+        '<tr><td>-</td></tr></table>'
+//    '<div class="patientImage" style="float: left; height: 125px; width: 114px; margin-right: 10px; background-size: cover; background-position: center center; background: #ddd; @include border-radius(3px); -webkit-box-shadow: inset 0 0 2px rgba(0,0,0,.6); background-image:url(theme/images/user/DefaultAvatar_small.jpg);"></div>',
+//    //'<div class="headshot" style="background-image:url(resources/images/headshots/{headshot});"></div>',
+//    '<span style="display: block; font-size: 12pt; font-weight: bold; color: #000;">ID: {id} - {rec_name}</strong>  - {[values.age.split(" ")\[0\]]} - {sex}&nbsp;</span>',
+//    '<span style="display: block; font-size: 12pt; font-weight: normal; color: #666;">{latestDiagnoseRecName}&nbsp;</span>',
+//    '<br /><br />'
 
 );
 
 
 var diagnoses = new Ext.XTemplate(
-    '<table id="box-table-a" summary="Employee Pay Sheet">',
+    '<br><h3>Diseases</h3>'+
+        '<table id="box-table-a" summary="Employee Pay Sheet" style="background-color: #FFFFFF; -webkit-border-radius: 6px";>',
         '<thead>',
             '<tr>',
                 '<th scope="col">Date</th>',
@@ -40,12 +42,12 @@ var diagnoses = new Ext.XTemplate(
             '<tpl for="diagnoseList">',
             '<tr>',
                 '<td>{diagnosed_date}</td>',
-                '<td>{[this.checkAct(value.is_active)]}</td>',
+                '<td>{[this.checkStatus(value.is_active)]}</td>',
                 '<td>{pathology_rec_name}</td>',
                 '<td>{disease_severity}</td>',
                 '<td>{healed_dated}</td>',
-                '<td>{is_infectious}</td>',
-                '<td>{is_allergy}</td>',
+                '<td>{[this.checkStatus(value.is_infecitous)]}</td>',
+                '<td>{[this.checkStatus(value.is_allergy)]}</td>',
             '</tr>',
             '</tpl>',
         '</tbody>',
@@ -55,8 +57,8 @@ var diagnoses = new Ext.XTemplate(
         // XTemplate configuration:
         disableFormats:true,
         // member functions:
-        checkAct:function (activness) {
-            if (activness == 'true') {
+        checkStatus:function (currentStat) {
+            if (currentStat == 'true') {
                 return '<input type="checkbox" checked="checked" />'
             }
             else return '<input type="checkbox" checked="unchecked" />'
@@ -131,7 +133,7 @@ Ext.define('NeqMobile.view.patient.Info', {
         },
 
         config:{
-
+            scrollable: true,
             styleHtmlContent:true,
             layout:'vbox',
             // layout:'card',
