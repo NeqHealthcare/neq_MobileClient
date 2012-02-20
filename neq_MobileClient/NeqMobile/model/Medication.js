16d7e51e246a8f7ae9e55ad8d32e3099a605c46a
J.G <jgansen@mail.uni-mannheim.de>
2012-02-20 21:12:21 +0100

    Finished Medication Model

--- neq_MobileClient/NeqMobile/model/Medication.js
+++ neq_MobileClient/NeqMobile/model/Medication.js
@@ -2,14 +2,35 @@
  * Created by J.G.
  */
 var myproxy = Ext.create('NeqMobile.proxy.NeqProxy',
-    {customUrl:'/medication/one'});
+    {customUrl:'/medication'});
 
 Ext.define('NeqMobile.model.Medication', {
     extend:'Ext.data.Model',
     config:{
         fields:[
-            'id'
-            //need input from backend team
+        'dose',
+        'route',
+        'duration_period',
+        'frequency_unit',
+        'dose_unit',
+        'frequency',
+        'indication',
+        'notes',
+        'is_active',
+        'admin_times',
+        'common_dosage',
+        'duration',
+        'form_rec_name',
+        'doctor_rec_name',
+        'route_rec_name',
+        'dose_unit_rec_name',
+        'indication_rec_name',
+        'common_dosage_rec_name',
+        'course_completed',
+        'discontinued',
+        'medicament_rec_name',
+        'start_treatment',
+        'end_treatment'
         ],
         proxy:myproxy
 
