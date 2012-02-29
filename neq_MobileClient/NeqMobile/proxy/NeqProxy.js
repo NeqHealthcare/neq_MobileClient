637cc37f0c731639d911d566296d0932f823e183
Christian T <c.theilemann@gmail.com>
2012-02-29 01:38:36 +0100

    slight design changes

--- neq_MobileClient/NeqMobile/proxy/NeqProxy.js
+++ neq_MobileClient/NeqMobile/proxy/NeqProxy.js
@@ -33,7 +33,7 @@ Ext.define('NeqMobile.proxy.NeqProxy', {
     },
     config:{
         customUrl:undefined,
-        withCredentials:true,
+      //  withCredentials:true,
         reader:{
             type:'json',
             root:'results'
