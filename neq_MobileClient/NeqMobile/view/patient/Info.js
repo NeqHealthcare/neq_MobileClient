fc279d0f73cadeb54962fe09d0f285a3a56dfb7b
J.G <jgansen@mail.uni-mannheim.de>
2012-03-06 02:10:05 +0100

    UX optimized :)

--- neq_MobileClient/NeqMobile/view/patient/Info.js
+++ neq_MobileClient/NeqMobile/view/patient/Info.js
@@ -32,20 +32,10 @@ var bulletRenderer = function(value, values){
     else return '<img src="../neq_MobileClient/bullet_red.png"/>'
 }
 
-var patientheader = new Ext.XTemplate(
-    '<h3>Patient</h3><table cellpadding="5" cellspacing="10" style="background-color: #FFFFFF; -webkit-border-radius: 6px";>' +
-        '<tr>' +
-        '<th rowspan="4" width=70><img src="theme/images/user/DefaultAvatar_small.jpg" width="60" height="67"></th>' +
-        '   <td align="left"><b>ID:</b> {id}</td></tr>' +
-        '<tr><td><b>Name:</b>{rec_name}</td></tr>' +
-        '<tr><td><b>Sex:</b>{sex}</td></tr>' +
-        '<tr><td><small>{latestDiagnoseRecName}</small></td></tr>' + '</table>'
-);
-
 Ext.define('NeqMobile.view.patient.Info', {
         extend:'Ext.Container',
         xtype:'patientInfo',
-        requires:['NeqMobile.view.patient.MedicationContainer','NeqMobile.view.patient.VaccinationContainer','NeqMobile.view.patient.DiagnoseContainer','NeqMobile.view.patient.detail.DiseaseDetail','NeqMobile.view.patient.detail.MedicationDetail','NeqMobile.view.patient.detail.VaccinationDetail'],
+        requires:['NeqMobile.view.patient.PatientHeader','NeqMobile.view.patient.MedicationContainer','NeqMobile.view.patient.VaccinationContainer','NeqMobile.view.patient.DiagnoseContainer','NeqMobile.view.patient.detail.DiseaseDetail','NeqMobile.view.patient.detail.MedicationDetail','NeqMobile.view.patient.detail.VaccinationDetail'],
 
         loadPatientHeader:function (patientrecord) {
             console.log('setting patients data...');
@@ -72,17 +62,18 @@ Ext.define('NeqMobile.view.patient.Info', {
             hidden: true,
             layout:'vbox',
             items:[
-                {xtype:'container',
-
-                    itemId:'patientheader',
-                    tpl:patientheader
+                {
+                    xtype:'patientheader'
                 },
-                {xtype:'diagnosescontainer'
+                {
+                    xtype:'diagnosescontainer'
                 }
                 ,
-                {xtype:'medicationscontainer'
+                {
+                    xtype:'medicationscontainer'
                 },
-                {xtype:'vaccinationscontainer'
+                {
+                    xtype:'vaccinationscontainer'
                 }
 
             ]
--- neq_MobileClient/NeqMobile/view/patient/PatientHeader.js
+++ neq_MobileClient/NeqMobile/view/patient/PatientHeader.js
@@ -1,3 +1,60 @@
-/**
- * Created by GSane
- */
+Ext.define('NeqMobile.view.patient.PatientHeader',{
+        extend:'Ext.form.FieldSet',
+        xtype:'patientheader',
+        config:{
+            title:'Patient',
+            itemId:'patientheader',
+            layout:'hbox',
+            items:[
+                {
+                    xtype: 'panel',
+                    layout:'vbox',
+                    padding: '10 10 10 10',
+                    width: '100%',
+                    items:[
+                        {
+                            xtype: 'panel',
+                            padding: '10 10 10 10',
+                            dock: 'left',
+                            html: '<img src="theme/images/user/DefaultAvatar_small.jpg" width="60" height="67">'
+                        },
+                        {
+                            xtype: 'textfield',
+                            padding: '5 5 5 5',
+                            labelWidth: '140px',
+                            label: 'Name: ',
+                            value: 'rec_name',
+                            readOnly: true
+                        },
+                        {
+                            xtype: 'textfield',
+                            padding: '5 5 5 5',
+                            labelWidth: '140px',
+                            label: 'Sex: ',
+                            value: 'sex',
+                            readOnly: true
+                        },
+                        {
+                            xtype: 'textfield',
+                            padding: '5 5 5 5',
+                            labelWidth: '140px',
+                            label: 'Latest Diagnose: ',
+                            value: 'latestDiagnoseRecName',
+                            readOnly: true
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+)
+
+
+
+
+
+
+
+
+
+
