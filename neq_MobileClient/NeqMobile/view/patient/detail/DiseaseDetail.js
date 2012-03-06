0d344b9d7c512e0d43a064c20306d5eaa019d8e4
Andreas Fleisch <and.fleisch@gmail.com>
2012-03-06 02:06:11 +0100

    detail view - need to be restructured

--- neq_MobileClient/NeqMobile/view/patient/detail/DiseaseDetail.js
+++ neq_MobileClient/NeqMobile/view/patient/detail/DiseaseDetail.js
@@ -1,21 +1,218 @@
 /**
  * Created with JetBrains WebStorm.
- * User: geekflyer
- * Date: 26.02.12
- * Time: 06:27
+ * User: chopsuey
+ * Date: 06.03.12
+ * Time: 02:27
  * To change this template use File | Settings | File Templates.
  */
 
-Ext.define('NeqMobile.view.patient.detail.DiseaseDetail',
-    {  extend:'Ext.form.Panel',
-        xtype:'diseasedetail',
-        config:{
-            scrollable:false,
-            items:[
-                {html:'<h2>Parteyyy!!</h2>'
-                    +'<h2>Parteyyy!!</h2>'
-                    + '<h2>Parteyyy!!</h2>' + '<h2>Parteyyy!!</h2>' + '<h2>Parteyyy!!</h2>' + '<h2>Parteyyy!!</h2>'
+
+Ext.define('NeqMobile.view.patient.detail.DiseaseDetail', {
+    extend: 'Ext.form.Panel',
+    xtype:'diseasedetail',
+
+    config: {
+        layout: {
+            type: 'vbox'
+        },
+        scrollable: false,
+        items: [
+            {
+                xtype: 'fieldset',
+                items: [
+                    {
+                        xtype: 'fieldset',
+                        title: 'Main',
+                        items: [
+                            {
+                                xtype: 'panel',
+                                layout: {
+                                    type: 'hbox'
+                                },
+                                items: [
+                                    {
+                                        xtype: 'panel',
+                                        flex: 1,
+                                        items: [
+                                            {
+                                                xtype: 'textfield',
+                                                label: 'Disease:'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'panel',
+                                        flex: 1,
+                                        items: [
+                                            {
+                                                xtype: 'datepickerfield',
+                                                label: 'Date of diagnosis:',
+                                                placeHolder: 'mm/dd/yyyy'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'panel',
+                                        flex: 1,
+                                        items: [
+                                            {
+                                                xtype: 'textfield',
+                                                label: 'Physician:'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                xtype: 'panel',
+                                layout: {
+                                    align: 'center',
+                                    type: 'hbox'
+                                },
+                                items: [
+                                    {
+                                        xtype: 'panel',
+                                        flex: 2,
+                                        items: [
+                                            {
+                                                xtype: 'selectfield',
+                                                label: 'Status of the disease:'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'panel',
+                                        flex: 2,
+                                        items: [
+                                            {
+                                                xtype: 'selectfield',
+                                                label: 'Severity:'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'panel',
+                                        flex: 1,
+                                        items: [
+                                            {
+                                                xtype: 'togglefield',
+                                                label: 'Infectious disease:'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        xtype: 'fieldset',
+                        title: 'Allergies',
+                        items: [
+                            {
+                                xtype: 'panel',
+                                layout: {
+                                    type: 'hbox'
+                                },
+                                items: [
+                                    {
+                                        xtype: 'panel',
+                                        flex: 4,
+                                        items: [
+                                            {
+                                                xtype: 'selectfield',
+                                                label: 'Allergy type:'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'panel',
+                                        flex: 1,
+                                        items: [
+                                            {
+                                                xtype: 'togglefield',
+                                                label: 'Allergic disease:'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        xtype: 'fieldset',
+                        title: 'Therapy',
+                        items: [
+                            {
+                                xtype: 'panel',
+                                layout: {
+                                    type: 'hbox'
+                                },
+                                items: [
+                                    {
+                                        xtype: 'panel',
+                                        flex: 2,
+                                        items: [
+                                            {
+                                                xtype: 'textfield',
+                                                label: 'Start of Treatment:'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'panel',
+                                        flex: 2,
+                                        items: [
+                                            {
+                                                xtype: 'textfield',
+                                                label: 'End of treatmeant:'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'panel',
+                                        flex: 1,
+                                        items: [
+                                            {
+                                                xtype: 'togglefield',
+                                                label: 'Currently on treatment:'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                xtype: 'panel',
+                                layout: {
+                                    type: 'hbox'
+                                },
+                                items: [
+                                    {
+                                        xtype: 'panel',
+                                        flex: 1,
+                                        items: [
+                                            {
+                                                xtype: 'textfield',
+                                                label: 'Treatment description:'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'panel',
+                                        flex: 1,
+                                        items: [
+                                            {
+                                                xtype: 'textfield',
+                                                label: 'Code:'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
                     }
-            ]
-        }}
-)
+                ]
+            }
+        ]
+    }
+
+});
