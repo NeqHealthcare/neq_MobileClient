77ee5ecf6a59d7c202e63e23f8304ac9c9c1eff1
Andreas Fleisch <and.fleisch@gmail.com>
2012-03-10 21:16:36 +0100

    diagnoses detail finished - formen angeordnet und fertiggestellt

--- neq_MobileClient/NeqMobile/view/patient/detail/DiseaseDetail.js
+++ neq_MobileClient/NeqMobile/view/patient/detail/DiseaseDetail.js
@@ -5,7 +5,27 @@
  * Time: 02:27
  * To change this template use File | Settings | File Templates.
  */
-
+/*
+Ext.define('NeqMobile.model.Diagnose', {
+    extend:'Ext.data.Model',
+    config:{
+        fields:[
+            'id',
+            'age',
+            'allergy_type',
+            {name:'date_start_treatment', type:'date',dateFormat:'time'},
+            {name:'diagnosed_date', type:'date',dateFormat:'time'},
+            'disease_severity',
+            'doctor',
+            'doctor_rec_name',
+            'extra_info',
+            {name:'healed_date', type:'date',dateFormat:'time'},
+            'is_active',
+            'is_allergy',
+            'is_infectious',
+            'is_on_treatment',
+            'pathology_rec_name'
+*/
 
 Ext.define('NeqMobile.view.patient.detail.DiseaseDetail', {
     extend: 'Ext.form.Panel',
@@ -22,83 +42,74 @@ Ext.define('NeqMobile.view.patient.detail.DiseaseDetail', {
                 items: [
                     {
                         xtype: 'fieldset',
+                        layout: {
+                            type: 'hbox'
+                        },
                         title: 'Main',
                         items: [
                             {
-                                xtype: 'panel',
-                                layout: {
-                                    type: 'hbox'
-                                },
+                                xtype: 'fieldset',
+                                margin: 10,
+                                flex: 1,
                                 items: [
                                     {
-                                        xtype: 'panel',
-                                        flex: 1,
-                                        items: [
-                                            {
-                                                xtype: 'textfield',
-                                                label: 'Disease:'
-                                            }
-                                        ]
+                                        xtype: 'textfield',
+                                        style: 'text-align: right;',
+                                        label: 'Disease:',
+                                        labelWidth: '57%',
+                                        readOnly: true
                                     },
                                     {
-                                        xtype: 'panel',
-                                        flex: 1,
-                                        items: [
-                                            {
-                                                xtype: 'datepickerfield',
-                                                label: 'Date of diagnosis:',
-                                                placeHolder: 'mm/dd/yyyy'
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        xtype: 'panel',
-                                        flex: 1,
-                                        items: [
-                                            {
-                                                xtype: 'textfield',
-                                                label: 'Physician:'
-                                            }
-                                        ]
+                                        xtype: 'selectfield',
+                                        height: 54,
+                                        style: 'text-align: right;',
+                                        label: 'Status of the disease:',
+                                        labelWidth: '57%',
+                                        readOnly: true
                                     }
                                 ]
                             },
                             {
-                                xtype: 'panel',
-                                layout: {
-                                    align: 'center',
-                                    type: 'hbox'
-                                },
+                                xtype: 'fieldset',
+                                margin: '10 10 10 0',
+                                flex: 1,
                                 items: [
                                     {
-                                        xtype: 'panel',
-                                        flex: 2,
-                                        items: [
-                                            {
-                                                xtype: 'selectfield',
-                                                label: 'Status of the disease:'
-                                            }
-                                        ]
+                                        xtype: 'datepickerfield',
+                                        style: 'text-align: right;',
+                                        label: 'Date of diagnosis:',
+                                        labelWidth: '50%',
+                                        placeHolder: 'mm/dd/yyyy',
+                                        readOnly: true
                                     },
                                     {
-                                        xtype: 'panel',
-                                        flex: 2,
-                                        items: [
-                                            {
-                                                xtype: 'selectfield',
-                                                label: 'Severity:'
-                                            }
-                                        ]
+                                        xtype: 'selectfield',
+                                        height: 54,
+                                        style: 'text-align: right;',
+                                        label: 'Severity:',
+                                        labelWidth: '50%',
+                                        readOnly: true
+                                    }
+                                ]
+                            },
+                            {
+                                xtype: 'fieldset',
+                                margin: '10 10 10 0',
+                                flex: 1,
+                                items: [
+                                    {
+                                        xtype: 'textfield',
+                                        style: 'text-align: right;',
+                                        label: 'Physician:',
+                                        labelWidth: '40%',
+                                        readOnly: true
                                     },
                                     {
-                                        xtype: 'panel',
-                                        flex: 1,
-                                        items: [
-                                            {
-                                                xtype: 'togglefield',
-                                                label: 'Infectious disease:'
-                                            }
-                                        ]
+                                        xtype: 'togglefield',
+                                        height: 54,
+                                        style: 'text-align: right;',
+                                        label: 'Infectious disease:',
+                                        labelWidth: '60%'
                                     }
                                 ]
                             }
@@ -106,33 +117,50 @@ Ext.define('NeqMobile.view.patient.detail.DiseaseDetail', {
                     },
                     {
                         xtype: 'fieldset',
+                        height: 110,
+                        layout: {
+                            type: 'hbox'
+                        },
                         title: 'Allergies',
                         items: [
                             {
-                                xtype: 'panel',
+                                xtype: 'fieldset',
+                                height: 52,
+                                margin: 10,
                                 layout: {
-                                    type: 'hbox'
+                                    type: 'default'
                                 },
+                                flex: 1,
                                 items: [
                                     {
-                                        xtype: 'panel',
-                                        flex: 4,
-                                        items: [
-                                            {
-                                                xtype: 'selectfield',
-                                                label: 'Allergy type:'
-                                            }
-                                        ]
-                                    },
+                                        xtype: 'selectfield',
+                                        height: 54,
+                                        style: 'text-align: right;',
+                                        label: 'Allergy type:',
+                                        labelWidth: '54%',
+                                        readOnly: true
+                                    }
+                                ]
+                            },
+                            {
+                                xtype: 'panel',
+                                flex: 1
+                            },
+                            {
+                                xtype: 'fieldset',
+                                height: 54,
+                                margin: '10 10 10 0',
+                                layout: {
+                                    type: 'default'
+                                },
+                                flex: 1,
+                                items: [
                                     {
-                                        xtype: 'panel',
-                                        flex: 1,
-                                        items: [
-                                            {
-                                                xtype: 'togglefield',
-                                                label: 'Allergic disease:'
-                                            }
-                                        ]
+                                        xtype: 'togglefield',
+                                        height: 54,
+                                        style: 'text-align: right;',
+                                        label: 'Allergic disease:',
+                                        labelWidth: '60%'
                                     }
                                 ]
                             }
@@ -140,71 +168,76 @@ Ext.define('NeqMobile.view.patient.detail.DiseaseDetail', {
                     },
                     {
                         xtype: 'fieldset',
+                        margin: '0 0 0 0',
+                        layout: {
+                            type: 'hbox'
+                        },
                         title: 'Therapy',
                         items: [
                             {
-                                xtype: 'panel',
+                                xtype: 'fieldset',
+                                margin: 10,
                                 layout: {
-                                    type: 'hbox'
+                                    type: 'default'
                                 },
+                                flex: 1,
                                 items: [
                                     {
-                                        xtype: 'panel',
-                                        flex: 2,
-                                        items: [
-                                            {
-                                                xtype: 'textfield',
-                                                label: 'Start of Treatment:'
-                                            }
-                                        ]
+                                        xtype: 'textfield',
+                                        height: 54,
+                                        style: 'text-align: right;',
+                                        label: 'Start of Treatment:',
+                                        labelWidth: '57%',
+                                        readOnly: true
                                     },
                                     {
-                                        xtype: 'panel',
-                                        flex: 2,
-                                        items: [
-                                            {
-                                                xtype: 'textfield',
-                                                label: 'End of treatmeant:'
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        xtype: 'panel',
-                                        flex: 1,
-                                        items: [
-                                            {
-                                                xtype: 'togglefield',
-                                                label: 'Currently on treatment:'
-                                            }
-                                        ]
+                                        xtype: 'textfield',
+                                        style: 'text-align: right;',
+                                        label: 'Treatment description:',
+                                        labelWidth: '57%',
+                                        readOnly: true
                                     }
                                 ]
                             },
                             {
-                                xtype: 'panel',
+                                xtype: 'fieldset',
+                                margin: '10 10 10 0',
                                 layout: {
-                                    type: 'hbox'
+                                    type: 'default'
                                 },
+                                flex: 1,
                                 items: [
                                     {
-                                        xtype: 'panel',
-                                        flex: 1,
-                                        items: [
-                                            {
-                                                xtype: 'textfield',
-                                                label: 'Treatment description:'
-                                            }
-                                        ]
+                                        xtype: 'textfield',
+                                        height: 54,
+                                        style: 'text-align: right;',
+                                        label: 'End of treatmeant:',
+                                        labelWidth: '50%',
+                                        readOnly: true
                                     },
                                     {
-                                        xtype: 'panel',
-                                        flex: 1,
-                                        items: [
-                                            {
-                                                xtype: 'textfield',
-                                                label: 'Code:'
-                                            }
-                                        ]
+                                        xtype: 'textfield',
+                                        style: 'text-align: right;',
+                                        label: 'Code:',
+                                        labelWidth: '50%',
+                                        readOnly: true
+                                    }
+                                ]
+                            },
+                            {
+                                xtype: 'fieldset',
+                                height: 54,
+                                margin: '10 10 0 0',
+                                layout: {
+                                    type: 'default'
+                                },
+                                flex: 1,
+                                items: [
+                                    {
+                                        xtype: 'togglefield',
+                                        height: 54,
+                                        label: 'Currently on treatment:',
+                                        labelWidth: '60%'
                                     }
                                 ]
                             }
