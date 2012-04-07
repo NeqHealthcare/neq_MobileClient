27376fc90a949c8bc8067b70edad8357a543a415
unknown <geekflyer@blubmobile.(none)>
2012-04-07 19:16:16 +0200

    major refactoring patientinfo patientdashboard patientlab

--- neq_MobileClient/app.js
+++ neq_MobileClient/app.js
@@ -11,8 +11,8 @@ Ext.application({
     name:'NeqMobile',
     requires:['Ext.ux.touch.grid.View','Ext.ux.touch.grid.feature.Expandable','Ext.DateExtras','NeqMobile.util.Renderer','NeqMobile.proxy.NeqProxy',
         'Ext.field.Password','Ext.Img','Ext.field.Select','Ext.Label','Ext.field.Search','Ext.field.DatePicker','Ext.field.Toggle'],
-    controllers:['General', 'Session', 'NeqMobile.controller.settings.Domains', 'Workspace','PatientDashboard','PatientDashboardLab','DoctorDashboard'],
-    views:['NeqMobile.view.settings.Domains', 'Viewport', 'NeqMobile.view.patient.PatientList', 'NeqMobile.view.patient.PatientDashboard','NeqMobile.view.doctor.DoctorDashboard', 'Workspace'],
+    controllers:['General', 'Session', 'NeqMobile.controller.settings.Domains', 'Workspace','PatientInfo','PatientLab','DoctorDashboard'],
+    views:['NeqMobile.view.settings.Domains', 'Viewport', 'NeqMobile.view.patient.PatientList', 'NeqMobile.view.patient.PatientInfo','NeqMobile.view.doctor.DoctorDashboard', 'Workspace'],
     stores:['Patients', 'Domains'],
     models:['Patient', 'Session', 'Domain','Diagnose'],
 
