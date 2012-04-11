1e48bb953f43ed82976103ae04aa47203dc06c7d
Andreas Fleisch <afleisch@hotmail.de>
2012-04-11 12:38:37 +0200

    adjusted config.rb and added default theme

--- neq_MobileClient/theme/scss/config.rb
+++ neq_MobileClient/theme/scss/config.rb
@@ -1,17 +1,23 @@
+
+
+
+
 # Get the directory that this configuration file exists in
-sass_path = File.dirname(__FILE__)
+dir = File.dirname(__FILE__)
 
 # Load the sencha-touch framework automatically.
-load File.join(sass_path, '..', '..', '..', 'st2', 'resources', 'themes')
+load File.join(dir, '..', '..', '..', 'st2', 'resources', 'themes')
 
-# Compass configurations
-css_path     = File.join(sass_path, "..", "css")
+# Look for any *.scss files in same directory as this file
+# Place compiled *.css files in the parent directory
+sass_path = dir
+css_path     = File.join(dir, "..", "css")
+output_style = :expanded
+environment  = :development
 
 # Require any additional compass plugins here
-images_dir = File.join(sass_path, "..", "images")
+images_dir = File.join(dir, "..", "images")
 
-environment  = :production
-sass_options = {:debug_info=>true} # by Compass.app 
-line_comments = true # by Compass.app 
+sass_options = {:debug_info=>true} # by Compass.app
+line_comments = true # by Compass.app
 
-output_style = :expanded # by Compass.app 
