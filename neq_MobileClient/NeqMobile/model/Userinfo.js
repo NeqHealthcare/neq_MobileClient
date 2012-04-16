2fc68e0c07b2ba6ad63695cb3b3fad2377efd164
Andreas Fleisch <afleisch@hotmail.de>
2012-04-16 21:07:08 +0200

    model aktualisiert auf den aktuellen webservice mit last_login und patient_number

--- neq_MobileClient/NeqMobile/model/Userinfo.js
+++ neq_MobileClient/NeqMobile/model/Userinfo.js
@@ -1,32 +1,19 @@
-
+/*
+ {"success":"true",
+ "data":[{"name":"Jan Gansen","id":"3","physician_id":"1","image_url":"http://i43.tinypic.com/29lzamh.png",
+ "number_of_patients":"5","last_login":"1334589916691"}]}
+ */
 Ext.define('NeqMobile.model.Userinfo', {
         extend:'Ext.data.Model',
         config: {
-            fields:['name', 'id', 'physician_id', 'image_url'],
+            fields:['name', 'id', 'physician_id', 'image_url', 'number_of_patients', 'last_login'],
 
             proxy: {
                 type:'neqproxy',
                 customUrl:'/user/personalInformation'
-            },
-
-            hasMany: 'NeqMobile.model.LastLogin'
+            }
         }
 
 
-    }
-);
-
-Ext.define('NeqMobile.model.LastLogin', {
-        extend:'Ext.data.Model',
-        config: {
-            fields: ['success', 'data'],
-
-            proxy: {
-                type:'neqproxy',
-                customUrl:'/usr/lastLogin'
-            },
-
-            belongsTo: 'NeqMobile.model.Userinfo'
-        }
     }
 );
